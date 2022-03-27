fa488dacbb6ccbbb5191b0644af13165323cdaf0
Josh Merlin <jmerlin.suf@gmail.com>
2022-03-27 14:41:48 +0800

    update api server to use contacts chaincode

--- TracePH-ContactTracing/drugman/javascript/apiserver.js
+++ TracePH-ContactTracing/drugman/javascript/apiserver.js
@@ -11,7 +11,7 @@ const fs = require('fs');
 const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
 const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
 
-app.get('/api/queryalldrugs', async function (req, res) {
+app.get('/api/contacts', async function (req, res) {
     try {
         // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
@@ -39,7 +39,7 @@ app.get('/api/queryalldrugs', async function (req, res) {
         // Evaluate the specified transaction.
         // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
         // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
-        const result = await contract.evaluateTransaction('queryAllDrugs');
+        const result = await contract.evaluateTransaction('queryAllContacts');
         console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
         res.status(200).json({response: result.toString()});
         // Disconnect from the gateway.
@@ -52,7 +52,7 @@ app.get('/api/queryalldrugs', async function (req, res) {
 });
 
 
-app.get('/api/query/:drug_index', async function (req, res) {
+app.get('/api/contact/:contact_id', async function (req, res) {
     try {
         // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
@@ -76,7 +76,7 @@ app.get('/api/query/:drug_index', async function (req, res) {
         // Evaluate the specified transaction.
         // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
         // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
-        const result = await contract.evaluateTransaction('queryDrug', req.params.drug_index);
+        const result = await contract.evaluateTransaction('queryContact', req.params.contact_id);
         console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
         return res.status(200).json({response: result.toString()});
         // await gateway.disconnect();
@@ -87,7 +87,7 @@ app.get('/api/query/:drug_index', async function (req, res) {
     }
 });
 
-app.post('/api/adddrug/', async function (req, res) {
+app.post('/api/contacts/', async function (req, res) {
     try {
         // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
@@ -111,7 +111,7 @@ app.post('/api/adddrug/', async function (req, res) {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('createDrug', req.body.drugid, req.body.drugname, req.body.timestamp, req.body.holder, req.body.location);
+        await contract.submitTransaction('createContact', req.body.contactid, req.body.timestamp, req.body.userid1, req.body.userid2);
         console.log('Transaction has been submitted');
         return res.send('Transaction has been submitted');
         // Disconnect from the gateway.
@@ -122,7 +122,7 @@ app.post('/api/adddrug/', async function (req, res) {
     }
 });
 
-app.put('/api/changeholder/:drug_index', async function (req, res) {
+app.put('/api/contact/:contact_id/timestamp', async function (req, res) {
     try {
         // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
@@ -149,7 +149,7 @@ app.put('/api/changeholder/:drug_index', async function (req, res) {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('changeDrugOwner', req.params.drug_index, req.body.holder);
+        await contract.submitTransaction('changeContactTimestamp', req.params.contact_id, req.body.timestamp);
         console.log('Transaction has been submitted');
         return res.send('Transaction has been submitted');
         // Disconnect from the gateway.
@@ -160,7 +160,7 @@ app.put('/api/changeholder/:drug_index', async function (req, res) {
     }
 });
 
-app.put('/api/changelocation/:drug_index', async function (req, res) {
+app.put('/api/contact/:contact_id/userid1', async function (req, res) {
     try {
         // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
@@ -187,7 +187,7 @@ app.put('/api/changelocation/:drug_index', async function (req, res) {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('changeDrugLocation', req.params.drug_index, req.body.location);
+        await contract.submitTransaction('changeContactUserId1', req.params.contact_id, req.body.userid);
         console.log('Transaction has been submitted');
         return res.send('Transaction has been submitted');
         // Disconnect from the gateway.
@@ -197,4 +197,43 @@ app.put('/api/changelocation/:drug_index', async function (req, res) {
         process.exit(1);
     }
 });
+
+app.put('/api/contact/:contact_id/userid2', async function (req, res) {
+    try {
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('drugs');
+        // Submit the specified transaction.
+        // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+        // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
+        await contract.submitTransaction('changeContactTimestamp', req.params.contact_id, req.body.userid);
+        console.log('Transaction has been submitted');
+        return res.send('Transaction has been submitted');
+        // Disconnect from the gateway.
+        // await gateway.disconnect();
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+});
+
 app.listen(8080);
