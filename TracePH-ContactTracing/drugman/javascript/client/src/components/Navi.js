7b157a22f2bb32196e191610ebae8c14e17b0935
Josh Merlin <jmerlin.suf@gmail.com>
2022-03-28 12:36:43 +0800

    client - temp. disable change tabs

--- TracePH-ContactTracing/drugman/javascript/client/src/components/Navi.js
+++ TracePH-ContactTracing/drugman/javascript/client/src/components/Navi.js
@@ -10,9 +10,9 @@ export class Navi extends Component {
         <Navbar.Brand href="/">Contacts</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/search">Search Contact</Nav.Link>
-          <Nav.Link href="/changeTimestamp">Change Timestamp</Nav.Link>
+          {/* <Nav.Link href="/changeTimestamp">Change Timestamp</Nav.Link>
           <Nav.Link href="/changeUserId1">Change User ID 1</Nav.Link>
-          <Nav.Link href="/changeUserId2">Change User ID 2</Nav.Link>
+          <Nav.Link href="/changeUserId2">Change User ID 2</Nav.Link> */}
           <Nav.Link href="/addContact">Add Contact</Nav.Link>
         </Nav>
       </Navbar>
