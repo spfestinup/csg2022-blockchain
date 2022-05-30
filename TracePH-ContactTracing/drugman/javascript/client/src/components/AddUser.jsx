b18811a335205b62169a584ae67abb55e970e9a5
Nanamsala <pdemanansala@gmail.com>
2022-05-30 10:04:07 +0800

    added basic validation on AddUser component

--- TracePH-ContactTracing/drugman/javascript/client/src/components/AddUser.jsx
+++ TracePH-ContactTracing/drugman/javascript/client/src/components/AddUser.jsx
@@ -34,7 +34,7 @@ export default function AddUser() {
       setLoading(true)
 
       const user = values
-      console.log(user)
+	  validate(user)
 
       const res = await API.addUser(user)
       console.log(res.data)
@@ -45,6 +45,42 @@ export default function AddUser() {
       setLoading(false)
       console.log(e)
     }
+  }
+	
+  const validate = (user) =>{
+      console.log(user)
+	  if(!validateLocation(user.location)){
+		throw new Error('Invalid Location.');
+	  }
+	  if(!validateEmail(user.email)){
+		throw new Error('Invalid Email.');
+	  }
+	  if(!validatePhone(user.phone)){
+		throw new Error('Invalid Phone Number.');
+	  }
+  }
+	
+  const validateLocation = (location) =>{
+	if((location >= 1000 && location <= 1090) || (location >= 2000 && location <= 2047) || (location >= 3000 && location <= 3012) || (location >= 4000 && location <= 4052)){
+		return true
+	}
+	return false
+  }
+  
+  const validateEmail = (email) =>{
+	const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if(!email || regex.test(email) === false){
+            return false;
+        }
+        return true;
+  }
+
+  const validatePhone = (phone) =>{
+	const regex = /^\d{10}$/;
+		  if(!phone || regex.test(phone) === false){
+            return false;
+        }
+        return true;
   }
 
   return (
